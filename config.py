9a97b45a3549e96e167aad40c217cdcddf65c000
opabdullahsid-ui <opabdullahsid@gmail.com>
2026-09-22 10:47:59 +0530

    Refactor config.py to use environment variables
    Updated config.py to fetch BOT_TOKEN from environment variables and changed ADMIN_IDS to a list.

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-# config.py
-BOT_TOKEN = "8855764092:AAFoz-81DXOddstuDPWglcrwsArAjBdzwKE"
-ADMIN_IDS = 1627494578,8958551425
+import os
+
+# Securely fetches the token from Render's Environment Variables
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+ADMIN_IDS = [1627494578]
 FILE_ID = "PASTE_YOUR_FILE_ID_HERE"
